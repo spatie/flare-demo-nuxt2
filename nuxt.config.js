e5a54573d20968921917be76ffd6b5995e811c34
Sam Apostel <sam@apostel.be>
2023-08-02 17:33:39 +0200

    tabs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,29 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt2-flare-demo',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'nuxt2-flare-demo',
+		htmlAttrs: {
+			lang: 'en',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+		{ src: '~/plugins/flare.client.ts' }
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
 	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 	buildModules: [
@@ -31,9 +33,9 @@ export default {
 		'@nuxtjs/tailwindcss',
 	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 }
